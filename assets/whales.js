ffb58c988b4ce30b806992047c3445545776603d
SarahRector <rector.sarah.r@gmail.com>
2020-07-13 14:49:56 -0700

    added product list

---
+++ assets/whales.js
@@ -0,0 +1,51 @@
+const whaleShark = {
+    id: 'whale-shark',
+    name: 'False Whale',
+    image: 'whale-shark.jpg',
+    description: 'This is not the whale you are looking for',
+    category: 'shark',
+    price: 5.00,
+    cost: 2.00
+};
+
+const blueWhale = {
+    id: 'blue-whale',
+    name: 'Blue Whale',
+    image: 'blue-whale.jpg',
+    description: 'Sadly, not as blue as I would have expected',
+    category: 'big-boi',
+    price: 25.00,
+    cost: 5.00
+};
+
+const humphreyWhale = {
+    id: 'humphrey-whale',
+    name: 'Humphrey the Wrong-Way Whale',
+    image: 'humphrey-whale.jpg',
+    description: 'Great whale, lousy sense of direction',
+    category: 'lost',
+    price: 15.00,
+    cost: 4.00
+};
+
+const mobyDickWhale = {
+    id: 'moby-dick-whale',
+    name: 'Moby Dick',
+    image: 'moby-dick.jpeg',
+    description: 'Some dude named Ahab is obsessed with me',
+    category: 'not-impressed',
+    price: 8.00,
+    cost: 3.00
+};
+
+const freeWillyWhale = {
+    id: 'free-willy-whale',
+    name: 'Free Willy',
+    image: 'free-willy.jpeg',
+    description: 'Honestly, kind of a show-off',
+    category: 'cocky',
+    price: 12.00,
+    cost: 8.00
+};
+
+export default wares;
