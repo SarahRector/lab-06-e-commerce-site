9fc8066c56c614e32804f90948750645608188c3
SarahRector <rector.sarah.r@gmail.com>
2020-07-15 15:30:14 -0700

    changed functionality of place order button

--- products/render-wares.js
+++ products/render-wares.js
@@ -45,7 +45,7 @@ export function renderWares(wares) {
         const stringyCart = JSON.stringify(cart);
         localStorage.setItem('CART', stringyCart);
         
-        alert('1' + wares.name + ' added to cart');
+        alert('1 ' + wares.name + ' added to cart');
     });
 
     pEl.append(buttonEl);
--- shopping-cart/shopping-cart.js
+++ shopping-cart/shopping-cart.js
@@ -5,6 +5,7 @@ import renderLineItem from './render-line-item.js';
 
 const tbody = document.querySelector('tbody');
 const orderTotalCell = document.getElementById('order-total-cell');
+const placeOrderButton = document.getElementById('place-order-button');
 const cart = getCart();
 
 for (let i = 0; i < cart.length; i++) {
@@ -17,3 +18,13 @@ for (let i = 0; i < cart.length; i++) {
 
 const orderTotal = calcOrderTotal(cart, wares);
 orderTotalCell.textContent = toUSD(orderTotal);
+
+if (cart.length === 0) {
+    placeOrderButton.disabled = true;
+} else {
+    placeOrderButton.addEventListener('click', () => {
+        localStorage.removeItem('CART');
+        alert('Order placed:\n' + JSON.stringify(cart, true, 2));
+        window.location = '../';
+    });
+}
